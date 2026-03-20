897791346d12a6a0c31ee0ad4061bfea73bb7fcc
samyak-19 <samlinkin19@gmail.com>
2026-03-20 15:29:52 +0530

    final fix signup + remove localhost

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 // import axios from "axios"
-import api from "../services/api.js"
+import API from "../services/api.js"
 import { Eye, EyeOff, Mail, Lock, User } from "lucide-react"
 
 function Signup({setPage}) {
